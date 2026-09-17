6ec2d250740094a79115c05845a6416f1129c652
sascheetz <sascheetz@gmail.com>
2026-09-17 14:41:09 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { writeFileSync } from 'fs';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'nojekyll',
+      writeBundle() {
+        writeFileSync(resolve(__dirname, 'dist', '.nojekyll'), '');
+      },
+    },
+  ],
   server: {
     host: true,
-  port: 5173,
+    port: 5173,
   },
   build: {
     outDir: 'dist',
